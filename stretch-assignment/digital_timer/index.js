aabfcb15ad57e4c3acccd270a23d3a1cb83b30e3
gebhartn <nicholas.gebhart@gmail.com>
2019-09-01 18:30:16 -0700

    Added comments, cleaned up codebase

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,4 +1,11 @@
-// create start button
+// Variables
+let msTens = document.getElementById("msTens");
+let msHundreds = document.getElementById("msHundreds");
+let ones = document.getElementById("secondOnes");
+let tens = document.getElementById("secondTens");
+let digits = document.querySelectorAll(".digit");
+
+// Create button function
 const createButton = str => {
   const node = document.createElement("div");
   node.style.textAlign = "center";
@@ -8,10 +15,12 @@ const createButton = str => {
   document.querySelector(".digits").appendChild(node);
 };
 
+// Show buttons on the DOM
 createButton("Start");
 createButton("Reset");
 const myButtons = document.querySelectorAll("button");
 
+// Start button
 myButtons[0].onclick = function() {
   let digits = document.querySelectorAll(".digit");
   digits.forEach(x => (x.style.color = "black"));
@@ -22,26 +31,33 @@ myButtons[0].onclick = function() {
   }, 10000);
 };
 
+// Reset button while not counting
+myButtons[1].onclick = function() {
+  tens.textContent = "-";
+  ones.textContent = "-";
+  msTens.textContent = "-";
+  msHundreds.textContent = "-";
+};
+
+// Start Timer function
 const startTimer = () => {
   let ms = 0;
-
+  // Set initial values to 0
   const countTime = setInterval(function() {
-    let msTens = document.getElementById("msTens");
-    let msHundreds = document.getElementById("msHundreds");
-    let ones = document.getElementById("secondOnes");
-    let tens = document.getElementById("secondTens");
-    let digits = document.querySelectorAll(".digit");
-
     tens.textContent = 0;
     ones.textContent = 0;
     msTens.textContent = 0;
     msHundreds.textContent = 0;
-
+    // Button to reset timer
     myButtons[1].onclick = function() {
-      ms = -10;
+      tens.textContent = "-";
+      ones.textContent = "-";
+      msTens.textContent = "-";
+      msHundreds.textContent = "-";
+      clearInterval(countTime);
       myButtons[0].disabled = false;
     };
-
+    // Compare lengths of milliseconds to string to print number
     if (ms.toString().length === 1) {
       msTens.textContent = ms.toString();
     }
@@ -56,7 +72,7 @@ const startTimer = () => {
       msHundreds.textContent = ms.toString()[1];
       msTens.textContent = ms.toString()[2];
     }
-
+    // Round up one millisecond to prevent clock from displaying: 09:98, etc.
     if (ms === 999) {
       tens.textContent = 1;
       ones.textContent = 0;
@@ -65,15 +81,6 @@ const startTimer = () => {
       digits.forEach(x => (x.style.color = "red"));
       clearInterval(countTime);
     }
-
-    if (ms === -10) {
-      tens.textContent = "-";
-      ones.textContent = "-";
-      msTens.textContent = "-";
-      msHundreds.textContent = "-";
-      clearInterval(countTime);
-    }
-
     ms++;
   }, 10);
 };
